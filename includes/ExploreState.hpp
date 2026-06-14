0fb95b4ed8de2c596d3e5cfee35ad4f788459fc3
OnkelGunnGunn <ebl022005@gmail.com>
2026-06-14 14:05:04 +0200

    Created ExploreState, Next step is implement battle state

---
+++ includes/ExploreState.hpp
@@ -0,0 +1,39 @@
+#ifndef EXPLORE_STATE_HPP
+#define EXPLORE_STATE_HPP
+
+#include "state.hpp"
+#include "monster.hpp"
+#include "game_data.hpp"
+
+enum ExploreOptions{
+    WILD_ENCOUNTER1 = 1,
+    WILD_ENCOUNTER2 = 2,
+    WILD_ENCOUNTER3 = 3,
+    EXIT_EXPLORE = 4,
+};
+
+
+class ExploreState : public State{
+
+    private:
+    GameData& data;
+    std::vector<Monster> wild_monster;
+    Monster encountered_monster;
+    
+
+
+    public:
+    ExploreState(GameData& data);
+    void on_entry() override;
+    int evaluate_user_input(int) override;
+    Monster random_monster();
+    Monster get_monster(int i) const;
+    void generate_random_encounters();
+
+
+
+};
+
+
+
+#endif
---
+++ src/ExploreState.cpp
@@ -0,0 +1,80 @@
+#include "ExploreState.hpp"
+
+
+
+
+ExploreState::ExploreState(GameData &data) : data(data)
+{
+}
+
+
+void ExploreState::on_entry()
+{
+    clear_screen();
+    
+    generate_random_encounters();
+
+    
+    std::cout << "You venture into the wild, on your path you encounter these Monsters, choose which one to fight: " << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "1. " << wild_monster[0].name << std::endl;
+    std::cout << "2. " << wild_monster[1].name << std::endl;
+    std::cout << "3. " << wild_monster[2].name << std::endl;
+    std::cout << std::endl;
+    std::cout << "4. Leave" << std::endl;
+
+}
+
+int ExploreState::evaluate_user_input(int user_input)
+{
+    // Switch state that activates battle state with the corresponding monster
+    int new_state;
+
+    switch (user_input)
+    {
+    case WILD_ENCOUNTER1:
+        wild_monster[0] = encountered_monster;
+        new_state = BATTLE;
+        break;
+
+    case WILD_ENCOUNTER2:
+        wild_monster[1] = encountered_monster;
+        new_state = BATTLE;
+        break;
+
+    case WILD_ENCOUNTER3:
+        wild_monster[2] = encountered_monster;
+        new_state = BATTLE;
+        break;
+
+    case EXIT_EXPLORE:
+        new_state = LEAVE;
+    
+    default:
+        new_state = EXPLORE;
+        break;
+    }
+    
+
+    return new_state;
+}
+
+Monster ExploreState::random_monster()
+{
+    return Monster("Horse", 4, 1);
+}
+
+Monster ExploreState::get_monster(int i) const
+{
+    return encountered_monster;
+}
+
+void ExploreState::generate_random_encounters(){
+    
+    wild_monster.clear();
+    wild_monster.resize(3);
+    wild_monster[0] = random_monster();
+    wild_monster[1] = random_monster();
+    wild_monster[2] = random_monster();
+}
